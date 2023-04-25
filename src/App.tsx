48ad1b0804b66a1ee39757c711adbb1ca42b68ee
PeterNelsonT <41645842+PeterNelsonT@users.noreply.github.com>
2023-04-25 08:34:43 +0000

    chore: scss changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import styles from './App.module.scss';
+
 const App = () => {
   const string = 'Hello';
-  return <div>{string}</div>;
+  return (
+    <div>
+      <div className={styles.app}>
+        <div className="stringprimary">{string}</div>
+        <div className="stringgreen">{string}</div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
